fc5e3a4e8cb19438d3d9ef34428a93ad6dd18b75
管恒 <guanheng@eastmoney.com>
2019-08-20 16:54:08 +0800

    add maven pom.xml and update gradle config

--- project-template-web/script/start.bat
+++ project-template-web/script/start.bat
@@ -1,2 +1,2 @@
 cd /d %~dp0
-cd .. && java -jar -Xms512m -Xmx512m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m ./apps/project-framework.jar
+cd .. && java -jar -Xms512m -Xmx512m -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m ./apps/project-template-web.jar

4aa5482776a22d7803c5a622dfc86e4f538a70c8
guanheng <guanheng@clinbrain.com>
2021-07-09 15:22:46 +0800

    delete gson from JsonParamResolver

--- project-template-web/src/main/java/com/eros/demo/common/support/JsonParamResolver.java
+++ project-template-web/src/main/java/com/eros/demo/common/support/JsonParamResolver.java
@@ -18,9 +18,8 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 /**
  * json映射到controller多参数解析器
@@ -30,105 +29,85 @@ import com.google.gson.GsonBuilder;
  */
 public class JsonParamResolver implements HandlerMethodArgumentResolver {
 
-	private final String JSON_PARAM = "JSON_PARAM";
-	private final Gson gson;
+    private final String JSON_PARAM = "JSON_PARAM";
 
-	public JsonParamResolver() {
-		gson = createDefaultObjectGson();
-	}
+    @Override
+    public boolean supportsParameter(MethodParameter param) {
+        return param.hasParameterAnnotation(JsonParam.class);
+    }
 
-	@Override
-	public boolean supportsParameter(MethodParameter param) {
-		return param.hasParameterAnnotation(JsonParam.class);
-	}
+    @Override
+    public Object resolveArgument(MethodParameter param, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws IOException, MissingServletRequestParameterException {
 
-	@Override
-	public Object resolveArgument(MethodParameter param, ModelAndViewContainer mavContainer,
-	                              NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws IOException, MissingServletRequestParameterException {
+        JSONObject json = getJsonObject(webRequest);
+        return parseObject(param, json);
+    }
 
-		JSONObject json = getJsonObject(webRequest);
-		return parseObject(param, json);
-	}
+    /**
+     * Build JSONObject with request.
+     * 
+     * @param webRequest
+     * @return
+     * @throws IOException
+     * @throws Exception
+     */
+    private JSONObject getJsonObject(NativeWebRequest webRequest) throws IOException {
+        String json = getJsonString(webRequest);
+        return JSONObject.parseObject(json);
+    }
 
-	/**
-	 * Initialize gson.
-	 * 
-	 * @return
-	 */
-	private Gson createDefaultObjectGson() {
-		GsonBuilder builder = new GsonBuilder();
-		builder.setDateFormat("yyyy-MM-dd HH:mm:ss")
-		       .setPrettyPrinting();
-		return builder.create();
-	}
+    /**
+     * Get json character string from request.
+     * 
+     * @param webRequest
+     * @return
+     * @throws IOException
+     * @throws Exception
+     */
+    private String getJsonString(NativeWebRequest webRequest) throws IOException {
+        String json = (String) webRequest.getAttribute(JSON_PARAM, RequestAttributes.SCOPE_REQUEST);
+        if (StringUtils.isEmpty(json)) {
+            HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+            HttpInputMessage inputMessage = new ServletServerHttpRequest(servletRequest);
+            json = IOUtils.toString(inputMessage.getBody(), StandardCharsets.UTF_8);
+            webRequest.setAttribute(JSON_PARAM, json, RequestAttributes.SCOPE_REQUEST);
+        }
 
-	/**
-	 * Build JSONObject with request.
-	 * 
-	 * @param webRequest
-	 * @return
-	 * @throws IOException
-	 * @throws Exception
-	 */
-	private JSONObject getJsonObject(NativeWebRequest webRequest) throws IOException {
-		String json = getJsonString(webRequest);
-		return JSONObject.parseObject(json);
-	}
+        return json;
+    }
 
-	/**
-	 * Get json character string from request.
-	 * 
-	 * @param webRequest
-	 * @return
-	 * @throws IOException
-	 * @throws Exception
-	 */
-	private String getJsonString(NativeWebRequest webRequest) throws IOException {
-		String json = (String) webRequest.getAttribute(JSON_PARAM, RequestAttributes.SCOPE_REQUEST);
-		if (StringUtils.isEmpty(json)) {
-			HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-			HttpInputMessage inputMessage = new ServletServerHttpRequest(servletRequest);
-			json = IOUtils.toString(inputMessage.getBody(), StandardCharsets.UTF_8);
-			webRequest.setAttribute(JSON_PARAM, json, RequestAttributes.SCOPE_REQUEST);
-		}
+    /**
+     * Convert from json character string to parameter object.
+     * 
+     * @param param
+     * @param json
+     * @return
+     * @throws MissingServletRequestParameterException
+     * @throws Exception
+     */
+    private Object parseObject(MethodParameter param, JSONObject json) throws MissingServletRequestParameterException {
+        JsonParam annotation = param.getParameterAnnotation(JsonParam.class);
+        String paramName = StringUtils.isEmpty(annotation.value()) ? param.getParameterName() : annotation.value();
+        String defaultValue = annotation.defaultValue();
+        String jsonValue = json == null ? null : json.getString(paramName);
+        if (ValueConstants.DEFAULT_NONE.equals(defaultValue) && StringUtils.isEmpty(jsonValue)) {
+            if (annotation.required()) {
+                throw new MissingServletRequestParameterException(paramName, param.getGenericParameterType().toString());
+            }
+            else {
+                return null;
+            }
+        }
+        else if (StringUtils.isEmpty(jsonValue)) {
+            jsonValue = defaultValue;
+        }
 
-		return json;
-	}
-
-	/**
-	 * Convert from json character string to parameter object.
-	 * 
-	 * @param param
-	 * @param json
-	 * @return
-	 * @throws MissingServletRequestParameterException
-	 * @throws Exception
-	 */
-	private Object parseObject(MethodParameter param, JSONObject json) throws MissingServletRequestParameterException {
-		JsonParam annotation = param.getParameterAnnotation(JsonParam.class);
-		String paramName = StringUtils.isEmpty(annotation.value()) ? param.getParameterName() : annotation.value();
-		String defaultValue = annotation.defaultValue();
-		String jsonValue = json == null ? null : json.getString(paramName);
-		if (ValueConstants.DEFAULT_NONE.equals(defaultValue) && StringUtils.isEmpty(jsonValue)) {
-			if (annotation.required()) {
-				throw new MissingServletRequestParameterException(paramName, param.getGenericParameterType().toString());
-			}
-			else {
-				return null;
-			}
-		}
-		else if (StringUtils.isEmpty(jsonValue)) {
-			jsonValue = defaultValue;
-		}
-
-		if (String.class.isAssignableFrom(param.getParameterType())) {
-			return jsonValue;
-		}
-		else if (JsonMap.class.isAssignableFrom(param.getParameterType())) {
-			// convert json for JsonMap
-			jsonValue = "{\"map\":" + jsonValue + "}";
-		}
-
-		return gson.fromJson(jsonValue, param.getGenericParameterType());
-	}
+        if (String.class.isAssignableFrom(param.getParameterType())) {
+            return jsonValue;
+        }
+        else {
+            return JSON.parseObject(jsonValue, param.getGenericParameterType());
+        }
+    }
 }
